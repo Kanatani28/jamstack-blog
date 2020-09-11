0325a804e0a7f153d645cfbcb4386ec9cf10f26f
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2020-09-11 16:41:42 +0900

    fix tag layout

--- components/atoms/Tag.tsx
+++ components/atoms/Tag.tsx
@@ -9,25 +9,23 @@ const Tag = ({
   textSize?: string;
 }): JSX.Element => (
   <Link href={`/tag/${tag.name}`}>
-    <a>
-      <span
-        className={classNamesBind([
-          "inline-block",
-          "bg-gray-200",
-          "hover:bg-gray-400",
-          "rounded-full",
-          "px-3",
-          "py-1",
-          "font-semibold",
-          "text-gray-700",
-          "mr-2",
-          "mb-2",
-          textSize || "text-sm",
-        ])}
-      >
-        {`#${tag.name}`}
-      </span>
-    </a>
+    <span
+      className={classNamesBind([
+        "inline-block",
+        "bg-gray-200",
+        "hover:bg-gray-400",
+        "rounded-full",
+        "px-3",
+        "py-1",
+        "font-semibold",
+        "text-gray-700",
+        "mr-2",
+        "mb-2",
+        textSize || "text-sm",
+      ])}
+    >
+      {`#${tag.name}`}
+    </span>
   </Link>
 );
 
